dc3cbf829dac7dad4ae295b72d43747db07c724d
dm <>
2025-04-06 11:08:23 +0300

    meh heh heh heh

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -44,6 +44,7 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("shoot"):
 		print($Camera3D/RayCast3D.get_collider())
+		spawn_bullet()
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
